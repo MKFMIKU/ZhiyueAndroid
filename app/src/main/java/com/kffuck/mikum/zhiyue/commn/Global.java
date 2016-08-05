7f98b921b2fc9c5b7490e4f6a699582c1c512d59
MKFMIKU <mikumkf@outlook.com>
2016-08-05 12:06:25 +0800

    Add pager

--- app/src/main/java/com/kffuck/mikum/zhiyue/commn/Global.java
+++ app/src/main/java/com/kffuck/mikum/zhiyue/commn/Global.java
@@ -1,7 +1,5 @@
 package com.kffuck.mikum.zhiyue.commn;
 
 public class Global {
-    public static final String HOST = "http://192.168.0.144:3000";
-
-
+    public static final String HOST = "http://192.168.0.100:3000/api";
 }
